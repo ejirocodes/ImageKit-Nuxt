ed380704d170e0b0295ff934972140b3cd439bce
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-03-08 11:06:40 +0100

    basic starter and config of imagekit

---
+++ plugins/vue-image-kit.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import ImageKit from "imagekitio-vue"
+
+Vue.use(ImageKit, {
+    urlEndpoint: "https://ik.imagekit.io/oe3ktrn8xgg/",
+    publicKey: "public_DenxSmV5k+FGGwqhog1VqWTE038=",
+  // authenticationEndpoint: "https://www.your-server.com/auth"
+})
